766981f8590d5d5c3d69c96e2fa7fc83f72d097d
Jack Frysinger <jcf0810@me.com>
2015-01-24 02:21:17 -0500

    Initial commit. Wrote Mission and Puzzle classes.

---
+++ LocationPuzzleGame/Mission/Mission.swift
@@ -0,0 +1,23 @@
+//
+//  Mission.swift
+//  LocationPuzzleGame
+//
+//  Created by Jack Frysinger on 1/24/15.
+//  Copyright (c) 2015 Jack Frysinger. All rights reserved.
+//
+
+import Foundation
+
+class Mission {
+    var puzzles: [Puzzle]
+    var puzzleIndex: Int
+    
+    init(puzzles: [Puzzle], puzzleIndex: Int) {
+        self.puzzles = puzzles;
+        self.puzzleIndex = puzzleIndex
+    }
+    
+    func resume() -> Puzzle {
+        return self.puzzles[puzzleIndex]
+    }
+}
