f0e82df39c8aec18ed081501fa7515c2b2ba4b84
Milo Davis <alicedavis@Milos-Computer.local>
2015-01-24 02:04:54 -0500

    Start of message class.  messageTest function added to appDelegate

---
+++ LocationPuzzleGame/messages.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// Represents a Message to the user
+protocol PMessage{
+    func deliver()
+    
+}
+// Message defined based
+class Message: PMessage{
+    var viewed = false
+    var delivered = false
+    //var timerStart
+    init(){
+        
+        
+    }
+    func deliver(){
+        //mission.unreadMessages += 1
+        
+    }
+  
+}
+
+func messageTest(){
+    var message = Message()
+    message.deliver()
+}
