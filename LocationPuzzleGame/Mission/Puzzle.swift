2bfdda18503f86ce81d0ca24ef38e4324cfe18ad
Jack Frysinger <jcf0810@me.com>
2015-01-24 09:50:33 -0500

    Replaced puzzle items with Hints. That will be the new terminology.

--- LocationPuzzleGame/Mission/Puzzle.swift
+++ LocationPuzzleGame/Mission/Puzzle.swift
@@ -10,33 +10,21 @@ import Foundation
 import CoreLocation
 
 class Puzzle {
-    var items: [PuzzleItem]
     var location: CLLocationCoordinate2D
     var hints: [Hint]
     
-    init(items: [PuzzleItem], location: CLLocationCoordinate2D, hints: [Hint]) {
-        self.items = items
+    init(location: CLLocationCoordinate2D, hints: [Hint]) {
         self.location = location
         self.hints = hints
     }
 }
 
-protocol PuzzleItem {
+class Hint {
+    var images: [String?]
+    var text: String
     
-}
-
-class TextPuzzleItem: PuzzleItem {
-    var item: String
-    
-    init(item: String) {
-        self.item = item
-    }
-}
-
-class ImagePuzzleItem: PuzzleItem {
-    var name: String
-    
-    init(name: String) {
-        self.name = name
+    init(images: [String?], text: String) {
+        self.images = images
+        self.text = text
     }
 }
